195fcafa281fae7f4c84173c441949e03cc4bc50
SHOAIBAHMED12842 <46914029+SHOAIBAHMED12842@users.noreply.github.com>
2019-01-26 15:14:23 +0500

    Add files via upload

---
+++ COCKTAIL SORT/cocktailsort/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace cocktailsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("ENTER THE LENGTH OF ELEMENTS:");  //NUMBER LENGTH.....
+                int numbers = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("ENTER THE MINIMUM RANGE:");          //MINIMUM RANGE....
+                int range1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("ENTER THE MAXIMUM RANGE:");          //MAXIMUM RANGE....
+                int range2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                COCKTAIL sort = new COCKTAIL();                    // CREATE OBJECT OF COCKTAIL CLASS...
+                int[] array = new int[numbers];                   // INITIALIZE AN ARRAY.....
+                array = sort.RANDOM(numbers, range1, range2);    // PASS LENGTH,MIN AND MAX TO RANDOM FUNCTION...
+                Console.WriteLine();
+                Console.WriteLine("RANDOM NUMBERS {0} UPTO MINIMUM:{1}:MAXIMUM:{2} ARE", numbers, range1, range2);
+                sort.print(array);                                                 // PRINT RANDOM NUMBERS.......
+                sort.Sorting(array);                                               // PASS ARRAY TO SORTING FUNCTION....
+                Console.WriteLine();
+                Console.WriteLine("AFTER APPLYING COCKTAIL SORT SORTED ELEMENT {1}:MAXIMUM:{2} ARE",numbers,range1,range2);
+                Console.WriteLine();
+                sort.print(array);                                                 // PRINT SORTED ARRAY....
+            }
+            catch (Exception E)                                        // EXCEPTION IF NOT ENTER DESIRE INPUT BY USER...
+            {
+                
+                Console.WriteLine("{0}",E.Message);                                                              
+            }
+            
+        }
+    }
+    class COCKTAIL
+    {
+        // TO GENERATE RANDOM NUMBERS
+        public int[] RANDOM(int n,int min,int max)        // NUMBERS,MIN,MAX
+        {
+            Random rond = new Random();                  // BUILT IN RANDOM CLASS IN VISUAL STUDIO C#
+
+            int[] RAND= new int[n];
+            for (int t = 0; t < RAND.Length; t++)
+            {
+                RAND[t] = rond.Next(min,max);
+            }
+            return RAND;
+        }
+        // TO PRINT ELEMENTS
+        public void print(int[] sort)                        
+        {
+            for (int t = 0; t < sort.Length; t++)
+            {
+                Console.WriteLine("{0} ELEMENT: {1}",t+1, sort[t]);
+            }
+        }
+        // FOR SORTING
+        public int[] Sorting(int[] element)
+        {
+            bool SORT = true;                 // INITIALIZES BOOL.....
+            int start = 0;                    // STARTING LENGTH......
+            int end = element.Length;         // ENDING LENGTH........
+
+            while (SORT == true)
+            {
+                        // RESET THE SORT FLAG ON ENTERING THE LOOP, BECAUSE IT COULD BE TRUE FROM ARRAY PREVIOUS ITERATION
+                SORT = false;
+                                 // LOOP FROM LEFT TO RIGHT TO COMPARE THE ELEMENTS SAME AS THE BUBBLE SORT......
+
+                for (int i = start; i < end - 1; ++i)     
+                {
+                    if (element[i] > element[i + 1])
+                    {
+                        // SWAP THE ELEMENT
+                        int temp = element[i];
+                        element[i] = element[i + 1];
+                        element[i + 1] = temp;
+                        SORT = true;
+                    }
+                }
+                                             // IF NOTHING MOVED, THEN ARRAY IS SORTED........
+                if (SORT == false)
+                {
+                    break;
+                }
+                    
+                                // OTHERWISE, RESET THE SORT FLAG SO THAT IT CAN BE USED IN THE NEXT COMPARISION.........
+                SORT = false;
+                                // MOVE THE END POINT BACK BY ONE, BECAUSE  ELEMENT AT THE END IS LARGEST..............
+                end = end - 1;
+
+                                // FROM RIGHT TO LEFT, AGAIN COMPARE ELEMENT AS IN BUBBLE SORT............
+                for (int t = end - 1; t >= start; t--)
+                {
+                    if (element[t] > element[t + 1])
+                    {
+                        // SWAP THE ELEMENT
+                        int temp = element[t];
+                        element[t] = element[t + 1];
+                        element[t + 1] = temp;
+                        SORT = true;
+                    }
+                }
+                                     // INCREASE THE STARTING POINT, BECAUSE  ELEMENT AT THE START IS SMALLEST....
+                start = start + 1;
+            }
+            return element;
+        }
+    }
+   
+}
---
+++ COMPARISONOFSORTS WITH TIME/COMPARISONOFSORTS/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace COMPARISIONOFSORTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long range;
+            Console.Write("ENTER HOW MANY NUMBERS ARE SORTED:");
+            range = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("");
+            rndom rndm = new rndom();
+            rndm.SortComparision(range);
+        }
+        public void Quick_Sort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    Quick_Sort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Quick_Sort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+        public int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+        public void cocktailSort(int[] a)
+        {
+            bool swapped = true;
+            int start = 0;
+            int end = a.Length;
+
+            while (swapped == true)
+            {
+
+                // reset the swapped flag on entering the 
+                // loop, because it might be true from a 
+                // previous iteration. 
+                swapped = false;
+
+                // loop from bottom to top same as 
+                // the bubble sort 
+                for (int i = start; i < end - 1; ++i)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                // if nothing moved, then array is sorted. 
+                if (swapped == false)
+                    break;
+
+                // otherwise, reset the swapped flag so that it 
+                // can be used in the next stage 
+                swapped = false;
+
+                // move the end point back by one, because 
+                // item at the end is in its rightful spot 
+                end = end - 1;
+
+                // from top to bottom, doing the 
+                // same comparison as in the previous stage 
+                for (int i = end - 1; i >= start; i--)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                // increase the starting point, because 
+                // the last stage would have moved the next 
+                // smallest number to its rightful spot. 
+                start = start + 1;
+            }
+        }
+    }
+    public class rndom
+    {
+        public void SortComparision(long n)
+        {
+            Random rond = new Random();
+
+            int[] ARRAY = new int[n];
+            for (int S = 0; S < ARRAY.Length; S++)
+            {
+                ARRAY[S] = rond.Next(100000);
+            }
+            //Console.WriteLine("INSERTION SORT");
+            Stopwatch timeI = new Stopwatch();
+            int variable;
+            timeI.Start();
+            for (int R = 0; R < ARRAY.Length - 1; R++)
+            {
+                for (int IN = R + 1; IN > 0; IN--)
+                {
+                    if (ARRAY[IN - 1] > ARRAY[IN])
+                    {
+                        variable = ARRAY[IN - 1];
+                        ARRAY[IN - 1] = ARRAY[IN];
+                        ARRAY[IN] = variable;
+                    }
+                }
+            }
+            timeI.Stop();
+            //Console.WriteLine("BUBBLESORT SORT");
+            Stopwatch timeB = new Stopwatch();
+            int variable1;
+            bool noswap = false;
+            timeB.Start();
+            for (int R = 0; R < ARRAY.Length - 1; R++)
+            {
+                for (int IN = 0; IN < ARRAY.Length - 1 - R; IN++)
+                {
+                    if (ARRAY[IN] > ARRAY[IN + 1])
+                    {
+                        variable1 = ARRAY[IN + 1];
+                        ARRAY[IN + 1] = ARRAY[IN];
+                        ARRAY[IN] = variable1;
+                        noswap = false;
+                    }
+                    if (noswap == true)
+                    {
+                        break;
+                    }
+                }
+            }
+            timeB.Stop();
+            //Console.WriteLine("SELECTION SORT");
+            Stopwatch timeS = new Stopwatch();
+            int variable2;
+            timeS.Start();
+            for (int R = 0; R < ARRAY.Length - 1; R++)
+            {
+                for (int IN = R + 1; IN < ARRAY.Length; IN++)
+                {
+                    if (ARRAY[R] > ARRAY[IN])
+                    {
+                        variable2 = ARRAY[IN];
+                        ARRAY[IN] = ARRAY[R];
+                        ARRAY[R] = variable2;
+                    }
+                }
+            }
+            timeS.Stop();
+            //Console.WriteLine("QUICK SORT");
+            Program quick = new Program();
+            Stopwatch timeQ = new Stopwatch();
+            timeQ.Start();
+            quick.Quick_Sort(ARRAY, 0, ARRAY.Length - 1);
+            timeQ.Stop();
+            Stopwatch timeC = new Stopwatch();
+            timeC.Start();
+            quick.cocktailSort(ARRAY);
+            timeC.Stop();
+            Console.WriteLine("Bubble Sort :{0}", timeB.Elapsed);
+            Console.WriteLine("Insertion Sort :{0}", timeI.Elapsed);
+            Console.WriteLine("Selection Sort :{0}", timeS.Elapsed);
+            Console.WriteLine("Quick Sort :{0}", timeQ.Elapsed);
+            Console.WriteLine("Cocktail shaker Sort :{0}", timeC.Elapsed);
+            Console.WriteLine("Bubble Sort :{0}", timeB.ElapsedMilliseconds);
+            Console.WriteLine("Insertion Sort :{0}", timeI.ElapsedMilliseconds);
+            Console.WriteLine("Selection Sort :{0}", timeS.ElapsedMilliseconds);
+            Console.WriteLine("Quick Sort :{0}", timeQ.ElapsedMilliseconds);
+            Console.WriteLine("Cocktail shaker Sort :{0}", timeC.ElapsedMilliseconds);
+        }
+
+    }
+}
